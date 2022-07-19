274122c8f80b5a11eeb587e45238b0b22d7cbbc1
Igor Araujo <igoralmeidaca@gmail.com>
2022-07-19 10:50:11 +0100

    test ledbar

--- ledbar.py
+++ ledbar.py
@@ -20,29 +20,10 @@ i = 0
  
 while True:
     try:
-        print("Test 1) Initialise - red to green")
-        # ledbar_init(pin,orientation)
-        # orientation: (0 = red to green, 1 = green to red)
-        grovepi.ledBar_init(ledbar, 0)
-        time.sleep(.5)
-
-
-        print("Test 2) Set level")
-        # ledbar_setLevel(pin,level)
-        # level: (0-10)
-        for i in range(0,11):
-            grovepi.ledBar_setLevel(ledbar, i)
-            time.sleep(.2)
-        time.sleep(.3)
-
         grovepi.ledBar_setLevel(ledbar, 8)
-        time.sleep(.5)
-
-        grovepi.ledBar_setLevel(ledbar, 2)
-        time.sleep(.5)
-
-        grovepi.ledBar_setLevel(ledbar, 5)
-        time.sleep(.5)        
+        time.sleep(5)
+        grovepi.ledBar_setLevel(ledbar, 1)
+        time.sleep(5)     
     except KeyboardInterrupt:
         grovepi.ledBar_setBits(ledbar, 0)
         break
