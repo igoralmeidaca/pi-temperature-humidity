4b416e967b8bd8b58734a798f85e7498f655579f
Igor Araujo <igoralmeidaca@gmail.com>
2022-07-19 12:18:33 +0100

    test ledbar

--- ledbar.py
+++ ledbar.py
@@ -4,19 +4,33 @@ import grovepi
 # Connect the Grove LED Bar to digital port D5
 # DI,DCKI,VCC,GND
 ledbar = 5
+
+unused = 0
+
+# pinMode() command format header
+pMode_cmd = [5]
+
+# Set level
+ledBarLevel_cmd = [52]
  
-grovepi.pinMode(ledbar,"OUTPUT")
+pinMode(ledbar,"OUTPUT")
 time.sleep(1)
 i = 0
- 
-# LED Bar methods
-# grovepi.ledBar_init(pin,orientation)
-# grovepi.ledBar_orientation(pin,orientation)
-# grovepi.ledBar_setLevel(pin,level)
-# grovepi.ledBar_setLed(pin,led,state)
-# grovepi.ledBar_toggleLed(pin,led)
-# grovepi.ledBar_setBits(pin,state)
-# grovepi.ledBar_getBits(pin)
 
-grovepi.ledBar_setLevel(ledbar, 1)
+ledBar_setLevel(ledbar, 1)
 
+# Grove LED Bar - set level
+# level: (0-10)
+def ledBar_setLevel(pin, level):
+	grovepi.write_i2c_block(ledBarLevel_cmd + [pin, level, unused])
+	grovepi.read_i2c_block(no_bytes = 1)
+	return 1
+
+# Setting Up Pin mode on Arduino
+def pinMode(pin, mode):
+	if mode == "OUTPUT":
+		grovepi.write_i2c_block(pMode_cmd + [pin, 1, unused])
+	elif mode == "INPUT":
+		grovepi.write_i2c_block(pMode_cmd + [pin, 0, unused])
+	grovepi.read_i2c_block(no_bytes = 1)
+	return 1
